22f02808781eaeff992108ce2184c54b6842fecb
vipin018 <vipinpathak018@gmail.com>
2025-03-20 13:23:35 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+
+// STATE MANAGEMENT (useState)
+
 // import React, { useState } from 'react'
 
 // const App = () => {
@@ -21,8 +24,8 @@
 
 
 
-// form handling
-
+// FORM HANDLING
+/*
 import React from 'react'
 
 
@@ -46,6 +49,63 @@ const App = () => {
           type="password" placeholder='password' />
         <button className='bg-green-500 text-white p-2 m-2 rounded-lg '
         type='submit'>submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
+*/
+
+// TWO WAY DATA BINDING
+
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setUsername('')
+    setEmail('')
+    setPassword('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}
+        className='flex flex-col items-center justify-center h-screen'>
+        <input
+          value={username}
+          className='border-2 border-gray-300 rounded-md p-2'
+          type="text"
+          onChange={(e) => {
+           setUsername(e.target.value)
+          }}
+
+          placeholder='name' />
+        <input
+          className='border-2 border-gray-300 rounded-md p-2'
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }}
+          placeholder='email' />
+        <input
+          className='border-2 border-gray-300 rounded-md p-2'
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value)
+          }}
+          placeholder='password' />
+        <button
+          className='bg-green-500 text-white p-2 m-2 rounded-lg '
+          type='submit'>submit</button>
       </form>
     </div>
   )
